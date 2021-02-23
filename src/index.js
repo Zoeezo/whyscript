a2198beb8598aaf9dbd62104d20228c0a4b07eb8
Zoeezo <zoey@eijgenberger.com>
2021-02-23 23:41:40 +0100

    Check if file is whyscript file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
+const { url } = require('inspector');
+
+function checkfile(fileURL) {
+    let urlArray = fileURL.split('\\');
+    let file = urlArray[urlArray.length - 1];
+
+    if(!file.includes('.ws')) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 function readFileData(fileURL) {
-    let data = fs.readFileSync(fileURL).toString();
+    let isWSFile = checkfile(fileURL);
 
+    if(!isWSFile) {
+        console.log("That isn't a WhyScript file!");
+        process.exit(1);
+    }
+
+    let data = fs.readFileSync(fileURL).toString();
     console.log(data);
 }
 
