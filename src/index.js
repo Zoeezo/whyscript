e866452f50c4b8e5f851896dc8feb698fecb4f27
Zoeezo <zoey@eijgenberger.com>
2021-02-24 01:57:59 +0100

    Start lexer done ig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const fs = require('fs');           // Import fs. (filesystem module from nodeJS)
 
+// Grammatical constants;
+const keywords = ['function', 'var'];
+const seperators = [';', '}', '{', '(', ')'];
+const parenthesis = ['\'', '"'];
+
 function checkfile(fileURL) {                   // Function to check if file url has .ws extension.
     let urlArray = fileURL.split('\\');         // Split the array by the \\ used in urls and stores it in the 'urlArray' variable.
     let file = urlArray[urlArray.length - 1];   // Get the last index from array, assumes that's the actual file and not a folder and stores it in the 'file' variable.
@@ -23,7 +28,7 @@ function readFileData(fileURL) {                        // Function to read file
 
     try {                                               // Try to.
         data = fs.readFileSync(fileURL).toString();     // Get the data from the file using the fs module, stores it in the 'data' variable.
-    } catch(err) {                                      // Catch errors
+    } catch(err) {                                      // Catch errors.
         console.log(err.message);                       // Log error message to console so the user knows what happened.
         process.exit(1);                                // Exit the process because we can't continue without data.
     }
@@ -31,6 +36,43 @@ function readFileData(fileURL) {                        // Function to read file
     return data;                                        // Returns the data.
 }
 
+function checkForKeywords(lines) {
+    let outLines = [];
+
+    for(let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        line = lines[lineIndex];
+
+        words = line.split(' ');
+
+        outWords = words;
+
+        for(let keywordIndex = 0; keywordIndex < keywords.length; keywordIndex++) {
+            let keyword = keywords[keywordIndex];
+
+            for(let wordIndex = 0; wordIndex < words.length; wordIndex++) {
+                let word = words[wordIndex];
+
+                if(word == keyword) {
+                    outWords[wordIndex] = {keyword: keyword};
+                }
+            }
+        }
+
+        outLines.push(outWords);
+    }
+
+    return outLines;
+}
+
+function lexer(data) {
+    let lines = data.split('\r\n');
+
+    lines = checkForKeywords(lines);
+
+    return lines;
+}
+
 let data = readFileData(process.argv[2]);       // Run readFileData function and stores the returned data in the 'data' variable.   
 
-console.log(data);                              // Log the data so we can check if we get the expected results.
+let lexedData = lexer(data);
+console.log(lexedData);
